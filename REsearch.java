6238c59532b930c8d333bde08a458723ca336eb3
GeorgeElstob <elstobgeorge@gmail.com>
2022-05-01 22:55:12 +1200

    final

--- REsearch.java
+++ REsearch.java
@@ -8,7 +8,7 @@ class REsearch {
             //parent varible
             private static int parentState;
             //lists to store the four components of each line
-            private static List<Integer> stateNum = new ArrayList<Integer>();
+            private static List<Integer> currStateNum = new ArrayList<Integer>();
             private static List<Character> ch = new ArrayList<Character>();
             private static List<Integer> next1 = new ArrayList<Integer>();
             private static List<Integer> next2 = new ArrayList<Integer>();
@@ -28,30 +28,30 @@ class REsearch {
         }    
         */
         //declearing variables
-        String line = " ";
+        String nextLine = " ";
         String[] split;
         String filename = args[0];
-        ArrayList<String> fileString = new ArrayList<>();
-        System.err.println("zzz");
+        ArrayList<String> textFile = new ArrayList<>();
+        
         
         // Read standard input to populate the FSM arrays
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.err.println("ccc");
-            line = reader.readLine();
-            System.out.println(line);
+  
+            nextLine = reader.readLine();
+            //System.out.println(line);
             int counter = 0;
 
-            while (line != null) {
-                System.err.println("c");
-                split = line.split("\\s+");
-                System.out.println(split[2]);
-                stateNum.add(Integer.parseInt(split[0]));
+            while (nextLine != null) {
+
+                split = nextLine.split(",");
+                //System.out.println(split[2]);
+                currStateNum.add(Integer.parseInt(split[0]));
                 ch.add(split[1].charAt(0));
                 next1.add(Integer.parseInt(split[2]));
                 next2.add(Integer.parseInt(split[3]));
 
-                line = reader.readLine();
+                nextLine = reader.readLine();
                 counter++;
             }
         } 
@@ -59,99 +59,92 @@ class REsearch {
             System.err.println("Invalid piped in FSM file. Try again");
             e.printStackTrace();
         }
-        System.err.println("yyy");
+     
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(filename))){
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
             
-            line = reader.readLine();
-            
+            nextLine = reader.readLine();
             // While there are still lines to read from the file
 
-            while (line != null) {
-                if (line.length() > 0) {
-                    fileString.add(line.trim());
-                }
-               
-                /* String[] splitChars = nextLine.split("");
-
-                 Check if the pattern matches the split chars
-                if (executePatternSearch(splitChars)) {
-                    System.err.println("Success: " + nextLine);
-                    System.out.println(nextLine);
-                } else {
-                    System.err.println("Failed: " + nextLine);
-                }
-                */
-                line = reader.readLine();
+            while (nextLine != null) {
+                if (nextLine.length() > 0) {
+                    // add to string
+                    textFile.add(nextLine.trim());
+                } 
+                nextLine = reader.readLine();
             }
+
             reader.close();
         }
         catch (Exception e) {
             System.err.println(e);
         } 
-        System.err.println("xxx");
-        //iterate through each of the lines
-        for (String nextLine : fileString) {
-            //create counters
-            int state = 0;
+        //System.out.println(textFile);
+
+        //for each to run through each line of
+        for (String line : textFile) {
+            //System.out.println(line);
+            int currState = 0;
             int symbol = 0;
 
-            parentState = next1.get(state);
+            parentState = next1.get(currState);
             //serach fsm and return true if found
-            if (searchLine(next1.get(state), nextLine, symbol)) {
-                //if true print 
-                System.out.println(nextLine);
+            if (searchLine(next1.get(currState), line, symbol)) {
+                //prints only if found
+                System.out.println(line);
             } 
         }
     }
     
-
-    //serach used regressivly 
-    public static boolean searchLine(int state, String line, int symbol)
+    
+    //search that is called regressively 
+    public static boolean searchLine(int currState, String line, int symbol)
     {
-        //if the end of the fsm is reached it means it won so return true
-        if (next1.get(state) == 0) {return true;}
-
-        //if the end of the line is reached it means it lost so return false
-        if (symbol >= line.length()) {return false;}
-
-        //get the character at this place
         char c = line.charAt(symbol);
 
-        //check if the node at this state has different nexts
-        if (next1.get(state) != next1.get(state)) {
+        if (next1.get(currState) == 0) {return true;}
 
-            parentState = next1.get(state);
+        if (symbol >= line.length()) {return false;}
 
-            if (searchLine(next1.get(state), line, symbol)) { return true; }
+  
+       
+        //if next1 differs for current node
+        if (next1.get(currState) != next1.get(currState)) {
+            //set parent state
+            parentState = next1.get(currState);
+            //calls self else reset parentstate
+            if (searchLine(next1.get(currState), line, symbol)) { return true; }
             else {
-                //System.err.println("Second branch");
-                parentState = next1.get(state);
-                if (searchLine(next1.get(state), line, symbol)) { return true; }
+
+                parentState = next1.get(currState);
+                //calls self
+                if (searchLine(next1.get(currState), line, symbol)) { return true; }
                 else { return false; }
             }
         }
         else {
-            //node has the same feet
-            //if node matches the character
-            if (ch.get(state) == c || ch.get(state) == '.') {
-                //increase character counter
-                symbol++;
-                //call itself recursively
-                if (searchLine(next1.get(state), line, symbol)) { return true; }
+            //if the charater is branching
+            if (ch.get(currState) == (char)3) {
+
+                //call self
+                if (searchLine(next1.get(currState), line, symbol)) { return true; }
             }
-            //check if the character is a branching one
-            else if (ch.get(state) == (char)3) {
-                //dont increace the character counter but call itself recursively
-                if (searchLine(next1.get(state), line, symbol)) { return true; }
+            //if both equal "."
+            else if (ch.get(currState) == c || ch.get(currState) == '.') {
+                symbol++;
+                //call self
+                if (searchLine(next1.get(currState), line, symbol)) { return true; }
             }
             else {
-                //increase the character counter
                 symbol++;
+                //call self using parent state instead
                 if (searchLine(parentState, line, symbol)) { return true; }
             }
         }
+        //if no regression happens
         return false;
     }
+    
 
 }   
